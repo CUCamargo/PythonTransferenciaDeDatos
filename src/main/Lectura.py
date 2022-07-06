7f8ea2bd9a7157431e7b3d25f56c2e95815b00d5
ccamargo31 <cezar9275@gmail.com>
2022-07-05 20:55:42 -0500

    VerFinal

--- src/main/Lectura.py
+++ src/main/Lectura.py
@@ -14,44 +14,41 @@ class LecturaArchivos:
                 self.palabras.append(lineas)
                 lineas = fichero.readline()
 
-            print(word)
-
             if len(word)>0:
                 tam = int(len(word))
                 tam = (tam/5)
             for i in range(int(tam)):
-                print(i)
                 if i >0:
                     self.empleado = {'surname': word[i*5+1], 'firstname': word[i*5]}
-                    respEmp = requests.post('http://localhost:8080/servicios/anadirempleado', data=self.empleado)
+                    respEmp = requests.post('http://localhost:8080/point/apiv1/clientes/add/empleado', data=self.empleado)
                     respEmp.json()
 
                     self.aeropuerto = {'name': word[i*5+4]}
-                    respAir = requests.post('http://localhost:8080/servicios/añadirairport', data=self.aeropuerto)
+                    respAir = requests.post('http://localhost:8080/point/apiv1/clientes/add/airport', data=self.aeropuerto)
                     respAir.json()
 
                     self.pais = {'name': word[i*5+2]}
-                    respPais = requests.post('http://localhost:8080/servicios/añadirpais', data=self.pais)
+                    respPais = requests.post('http://localhost:8080/point/apiv1/clientes/add/pais', data=self.pais)
                     respPais.json()
 
                     self.idioma = {'name': word[i*5+3]}
-                    respIdiom = requests.post('http://localhost:8080/servicios/añadirlenguajes', data=self.idioma)
+                    respIdiom = requests.post('http://localhost:8080/point/apiv1/clientes/add/lenguaje', data=self.idioma)
                     respIdiom.json()
                 else:
                     self.empleado = {'surname': word[i+1], 'firstname': word[i+0]}
-                    respEmp = requests.post('http://localhost:8080/servicios/anadirempleado', data=self.empleado)
+                    respEmp = requests.post('http://localhost:8080/point/apiv1/clientes/add/empleado', data=self.empleado)
                     respEmp.json()
 
                     self.aeropuerto = {'name': word[i+4]}
-                    respAir = requests.post('http://localhost:8080/servicios/añadirairport', data=self.aeropuerto)
+                    respAir = requests.post('http://localhost:8080/point/apiv1/clientes/add/airport', data=self.aeropuerto)
                     respAir.json()
 
                     self.pais = {'name': word[i+2]}
-                    respPais = requests.post('http://localhost:8080/servicios/añadirpais', data=self.pais)
+                    respPais = requests.post('http://localhost:8080/point/apiv1/clientes/add/pais', data=self.pais)
                     respPais.json()
 
                     self.idioma = {'name': word[i+3]}
-                    respIdiom = requests.post('http://localhost:8080/servicios/añadirlenguajes', data=self.idioma)
+                    respIdiom = requests.post('http://localhost:8080/point/apiv1/clientes/add/lenguaje', data=self.idioma)
                     respIdiom.json()
 
             #else:print("El documento no tiene informarción")
